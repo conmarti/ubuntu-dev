b94c9176f35f8a9717196cd3d87856f129a7178e
ulkoenig <ulrich.koenig@telekom.de>
2018-08-23 08:44:40 +0000

    fix postman

--- src/ubuntu/install/postman.sh
+++ src/ubuntu/install/postman.sh
@@ -6,4 +6,4 @@ echo "Install Postman"
 wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
 tar -xzf postman.tar.gz -C /usr/local/etc/
 rm postman.tar.gz
-sudo ln -s /usr/local/etc/Postman/Postman /usr/local/bin/postman
+ln -s /usr/local/etc/Postman/Postman /usr/local/bin/postman
