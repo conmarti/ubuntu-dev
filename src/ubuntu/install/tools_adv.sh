e012e3c7a5c6dcc16aa471f0cb22a32e31f474dc
ulkoenig <ulrich.koenig@telekom.de>
2018-08-23 11:18:28 +0000

    remove intelliJ

--- src/ubuntu/install/tools_adv.sh
+++ src/ubuntu/install/tools_adv.sh
@@ -4,5 +4,5 @@ set -e
 
 echo "Install some more developer tools"
 apt-get update 
-apt-get install -y dnsutils inetutils-traceroute gedit eclipse netbeans intellij-idea-community
+apt-get install -y dnsutils inetutils-traceroute gedit eclipse netbeans
 apt-get clean -y
