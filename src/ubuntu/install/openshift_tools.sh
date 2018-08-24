a04e33e957edb91e60be02055169ddc81c3e8a3b
ulkoenig <ulrich.koenig@telekom.de>
2018-08-24 16:34:44 +0000

    test oc tools

--- src/ubuntu/install/openshift_tools.sh
+++ src/ubuntu/install/openshift_tools.sh
@@ -3,8 +3,8 @@
 set -e
 echo "Install OC Client"
 
-wget https://github.com/openshift/origin/releases/download/v3.7.1/openshift-origin-client-tools-v3.7.1-ab0f056-linux-64bit.tar.gz -O /usr/local/etc/openshift.tar.gz
+wget https://github.com/openshift/origin/releases/download/v3.7.1/openshift-origin-client-tools-v3.7.1-ab0f056-linux-64bit.tar.gz -O openshift.tar.gz
 
-gunzip /usr/local/etc/openshift.tar.gz
-tar -xvf /usr/local/etc/openshift.tar
-ln -s /usr/local/etc/openshift/oc /usr/local/bin/oc
+tar -xzf openshift.tar.gz -C /usr/local/etc/
+rm openshift.tar.gz
+ln -s /usr/local/etc/openshift-origin-client-tools-v3.7.1-ab0f056-linux-64bit/oc /usr/local/bin/oc
