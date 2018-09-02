ada78b938106281a5bf1eff0f3f260d0c21389a6
ulkoenig <ulrich.koenig@telekom.de>
2018-09-02 09:53:24 +0000

    add some menu and desktp item. Delete all centos staff

--- src/ubuntu/install/eclipse.sh
+++ src/ubuntu/install/eclipse.sh
@@ -6,4 +6,6 @@ echo 'Installing eclipse'
 wget http://ftp.fau.de/eclipse/technology/epp/downloads/release/photon/RC3/eclipse-jee-photon-RC3-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
 tar -xzf eclipse.tar.gz -C /usr/local/etc/
 rm eclipse.tar.gz
+ln -s /usr/local/etc/eclipse/eclipse /usr/local/bin/eclipse
+
 
