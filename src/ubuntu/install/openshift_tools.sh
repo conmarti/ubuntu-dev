4824f586bf22b613923300e304143cd048945ce6
Ulrich König <ulrich.koenig@telekom.de>
2018-12-06 13:10:52 +0100

    Fix helm installation in openshift_tools.sh

--- src/ubuntu/install/openshift_tools.sh
+++ src/ubuntu/install/openshift_tools.sh
@@ -10,7 +10,7 @@ rm openshift.tar.gz
 ln -s /usr/local/etc/openshift-origin-client-tools-v3.7.1-ab0f056-linux-64bit/oc /usr/local/bin/oc
 
 echo "Install Helm"
-curl -s https://storage.googleapis.com/kubernetes-helm/helm-v2.9.0-linux-amd64.tar.gz
+wget https://storage.googleapis.com/kubernetes-helm/helm-v2.9.0-linux-amd64.tar.gz -O helm-v2.9.0-linux-amd64.tar.gz 
 tar -xzf helm-v2.9.0-linux-amd64.tar.gz -C /usr/local/etc/
 rm helm-v2.9.0-linux-amd64.tar.gz
 ln -s /usr/local/etc/linux-amd64/helm /usr/local/bin/helm
